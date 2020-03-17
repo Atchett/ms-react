cc73986bbe2cd4a6938b6149d6e99caa3eddf86a
Atchett <Atchett@users.noreply.github.com>
2020-03-17 20:34:56 +0000

    trying useEffect

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import classes from "./Cockpit.css";
 
 const Cockpit = props => {
+  useEffect(() => {
+    console.log("[Cockpit.js] useEffect");
+    setTimeout(() => {
+      alert("some alert");
+    }, 1000);
+  }, []);
+
   const assignedClasses = [];
   let btnClass = "";
   if (props.showPeople) {
